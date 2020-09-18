2fe337d1d853dd83753203dfea7897bb92a6cc28
LuatNguyenn <luat.nguyen@edu.sait.ca>
2020-09-17 23:35:30 -0600

    2

--- src/Search.js
+++ src/Search.js
@@ -1,11 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Search.css";
+import { DateRangePicker } from "react-date-range";
+//Date picker component
 function Search() {
-  return 
-  
-  <div className='search'>
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
+  const selectionRange = {
+    startDate: startDate,
+    endDate: endDate,
+    key: "selection",
+  };
 
-  </div>;
+  function handleSelect(range) {
+    setStartDate(range.selection.startDate);
+    setEndDate(range.selection.endDate);
+  };
+
+  return (
+    <div className='search'>
+      <DateRangePicker range={[selectionRange]} onChange={handleSelect} />
+    </div>
+  )
 }
 
 export default Search;
